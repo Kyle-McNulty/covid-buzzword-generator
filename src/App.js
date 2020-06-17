c499d8eb4f6732ada5385af1e388205f4899d7e4
Kyle-McNulty <kylemcnulty@gmail.com>
2020-06-16 22:39:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const ADJECTIVES = ["new", "unprecedented", "modern", "challenging"]
+const NOUNS = ["reality", "future", "era", "time"]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phrase: ""
+    };
+    this.generatePhrase = this.generatePhrase.bind(this);
+    this.getRandomIntInclusive = this.getRandomIntInclusive.bind(this);
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
+
+  generatePhrase(event) {
+    event.preventDefault();
+    const adjective = ADJECTIVES[this.getRandomIntInclusive(0, ADJECTIVES.length - 1)];
+    const noun = NOUNS[this.getRandomIntInclusive(0, NOUNS.length - 1)];
+    const newPhrase = adjective + " " + noun;
+    this.setState({
+      phrase: newPhrase
+    })
+  }
+  
+  render() {
+    return (<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click the button to randomly generate some fun COVID-19 buzz-phrases for your marketing needs!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.generatePhrase}>
+          Start my mindless marketing!
+        </button>
+        <h1>Your Phrase:</h1>
+        <p>{this.state.phrase}</p>
       </header>
-    </div>
-  );
+    </div>);
+  }
 }
+
+
 
 export default App;
